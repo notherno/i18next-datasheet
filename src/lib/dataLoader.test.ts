c9daee76b7b6ea967e3af4cfcbe3747cacfe7fdf
notherno <kitanow.takaaki@gmail.com>
2018-06-11 01:02:32 +0900

    Improve test

--- src/lib/dataLoader.test.ts
+++ src/lib/dataLoader.test.ts
@@ -1,17 +1,45 @@
 import { extractBundles } from './dataLoader'
 
-const en = {
+const bundle = {
   en: {
     greetings: {
       hello: 'hi',
+    },
+  },
+  ja: {
+    greetings: {
+      hello: 'やあ',
     },
   },
 }
 
 describe('extractBundle', () => {
   it('loads resource bundles', () => {
-    const module = extractBundles(en, 'en')
+    const module = extractBundles(bundle, 'en')
+
+    const expected = {
+      key: 'root',
+      modules: [
+        {
+          key: 'greetings',
+          modules: [],
+          texts: [
+            {
+              key: 'hello',
+              values: {
+                en: 'hi',
+                ja: 'やあ',
+              },
+            },
+          ],
+        },
+      ],
+      texts: [],
+    }
 
     expect(module.key).toBe('root')
+    expect(JSON.stringify(module, null, '  ')).toBe(
+      JSON.stringify(expected, null, '  '),
+    )
   })
 })
