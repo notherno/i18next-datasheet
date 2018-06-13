551a468d821ce34cd38df43eff1fd46802a0773f
notherno <kitanow.takaaki@gmail.com>
2018-06-14 01:25:09 +0900

    Add type definitions

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,16 @@ declare module 'react-dev-utils/*' {
   const module: any
   export = module
 }
+
+declare module 'raf' {
+  function polyfill(scope: any): void
+}
+
+declare module 'promise/lib/rejection-tracking' {
+  function enable(): void
+}
+
+declare module 'object-assign' {
+  const assign: typeof Object.assign
+  export = assign
+}
