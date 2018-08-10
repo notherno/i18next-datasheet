68f0b34ae4048d2d2eda66dae501bb7fb6d5c84e
notherno <kitanow.takaaki@gmail.com>
2018-08-10 17:48:44 +0900

    Load config from file

--- config/routes.ts
+++ config/routes.ts
@@ -3,6 +3,28 @@ import express from 'express'
 import fs from 'fs'
 import path from 'path'
 import util from 'util'
+import { fromYAML, toJSON, toYAML } from '../src/lib/formatter'
+
+interface AppConfig {
+  sourceDir: string
+  resources: Array<{
+    name: string
+    file: string
+    type: 'json' | 'yaml'
+  }>
+}
+
+function loadConfig() {
+  try {
+    return require('../config.json') as AppConfig
+  } catch (e) {
+    return require('../config.sample.json') as AppConfig
+  }
+}
+
+function resolvePath(sourceDir: string, filename: string) {
+  return path.resolve(__dirname, '..', sourceDir, filename)
+}
 
 const router = express.Router()
 
@@ -10,36 +32,45 @@ router.use(bodyParser.json())
 
 router
   .route('/data')
-  .get(async (req, res) => {
-    const langs = ['en', 'de', 'ja']
+  .get(async (_, res) => {
+    const config = loadConfig()
+
     const bundle = (await Promise.all(
-      langs.map(lang => {
-        return util.promisify(fs.readFile)(
-          path.resolve('data', `${lang}.json`),
-          {
-            encoding: 'utf-8',
-          },
-        )
+      config.resources.map(resource => {
+        const filePath = resolvePath(config.sourceDir, resource.file)
+
+        return util.promisify(fs.readFile)(filePath, {
+          encoding: 'utf-8',
+        })
       }),
     ))
-      .map(value => JSON.parse(value))
+      .map(value => fromYAML(value))
       .reduce(
-        (data, value, index) => ({ ...data, ...{ [langs[index]]: value } }),
+        (data, value, index) => ({
+          ...data,
+          ...{ [config.resources[index].name]: value },
+        }),
         {},
       )
 
-    res.json({ langs, bundle })
+    res.json({ langs: config.resources.map(resource => resource.name), bundle })
   })
   .post(async (req, res) => {
     const data = req.body
+    const config = loadConfig()
 
-    const langs = Object.keys(data)
+    await config.resources.map(resource => {
+      const filePath = resolvePath(config.sourceDir, resource.file)
 
-    await langs.map(lang => {
-      return util.promisify(fs.writeFile)(
-        path.resolve('data', `${lang}.json`),
-        `${JSON.stringify(data[lang], null, '  ')}\n`,
-      )
+      const bundle = data[resource.name]
+
+      if (bundle == null) {
+        throw new Error(`Data "${resource.name}" does not exist`)
+      }
+
+      const content = resource.type === 'json' ? toJSON(bundle) : toYAML(bundle)
+
+      return util.promisify(fs.writeFile)(filePath, content)
     })
     res.json({ ok: true })
   })
